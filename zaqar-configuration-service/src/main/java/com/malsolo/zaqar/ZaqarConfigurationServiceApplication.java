358902039f3586b6ca547952195db803ae7f00c4
Javier Beneito Barquero <jbbarquero@gmail.com>
2015-11-18 05:57:39 +0100

    Zaqar: project structure and configuration service

---
+++ zaqar-configuration-service/src/main/java/com/malsolo/zaqar/ZaqarConfigurationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.malsolo.zaqar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaqarConfigurationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZaqarConfigurationServiceApplication.class, args);
+    }
+}
