7f152f312b50d71a90becf91d100d6d6abaf8086
Javier Beneito Barquero <jbbarquero@gmail.com>
2015-11-18 06:41:13 +0100

    Zaqar: configuration service done.
    Plus config server git uri

--- zaqar-configuration-service/src/main/java/com/malsolo/zaqar/configuration/service/ZaqarConfigurationServiceApplication.java
+++ zaqar-configuration-service/src/main/java/com/malsolo/zaqar/configuration/service/ZaqarConfigurationServiceApplication.java
@@ -1,9 +1,11 @@
-package com.malsolo.zaqar;
+package com.malsolo.zaqar.configuration.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ZaqarConfigurationServiceApplication {
 
     public static void main(String[] args) {
