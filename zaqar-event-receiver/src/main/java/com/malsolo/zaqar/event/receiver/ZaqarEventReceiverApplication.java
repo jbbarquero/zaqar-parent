0fe75860304375f6480c19f2236d94aab9aad54a
Javier Beneito Barquero <jbbarquero@gmail.com>
2015-11-23 06:39:56 +0100

    Zaqar: event receiver almost done (isolated for the time being)

--- zaqar-event-receiver/src/main/java/com/malsolo/zaqar/event/receiver/ZaqarEventReceiverApplication.java
+++ zaqar-event-receiver/src/main/java/com/malsolo/zaqar/event/receiver/ZaqarEventReceiverApplication.java
@@ -2,8 +2,10 @@ package com.malsolo.zaqar.event.receiver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ZaqarEventReceiverApplication {
 
     public static void main(String[] args) {
